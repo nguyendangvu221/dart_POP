fd716121e73425ff388e40b4f77b7e6f77cba354
nguyendangvu221 <nguyendangvu221@gmail.com>
2022-07-20 00:07:20 +0700

    Update exercies1.dart

--- exercies1.dart
+++ exercies1.dart
@@ -7,10 +7,22 @@ void inputInfo(int n, List<int> a) {
   }
 }
 
-void sort(int n, List<int> a) {
+void sortAscending(int n, List<int> a) {
   for (int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
       if (a[i] > a[j]) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
+}
+
+void sortDecrease(int n, List<int> a) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (a[i] < a[j]) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
@@ -23,6 +35,6 @@ void main(List<String> args) {
   int n = int.parse(stdin.readLineSync()!);
   List<int> a = [];
   inputInfo(n, a);
-  sort(n, a);
+  sortAscending(n, a);
   print(a);
 }
